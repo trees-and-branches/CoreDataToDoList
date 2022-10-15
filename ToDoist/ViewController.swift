f7c331fbf5a4ff45df22afef715f858b3933dd48
Parker <parkerrushton@gmail.com>
2022-10-15 16:29:53 -0600

    Initial Commit

---
+++ ToDoist/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoist
+//
+//  Created by Parker Rushton on 10/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
