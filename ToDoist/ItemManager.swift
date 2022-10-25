2ef4b0921f2666dad1c53c9db874fb595a13ce5d
Parker <parkerrushton@gmail.com>
2022-10-25 16:43:04 -0600

    more core data prep

--- ToDoist/ItemManager.swift
+++ ToDoist/ItemManager.swift
@@ -39,18 +39,5 @@ class ItemManager {
         guard let index = allItems.firstIndex(of: item) else { return }
         allItems.remove(at: index)
     }
-    
-    // Mock Data
-    
-    func loadMockData() {
-        let yesterday = Calendar.current.date(byAdding: .day, value: -1, to: Date())!
-        let twoDaysAgo = Calendar.current.date(byAdding: .day, value: -2, to: Date())!
-        let tenMinutesAgo = Calendar.current.date(byAdding: .minute, value: -10, to: Date())!
-        let mock1 = Item(id: UUID().uuidString, title: "Make my bed", createdAt: Date(), completedAt: nil)
-        let mock2 = Item(id: UUID().uuidString, title: "Take out the trash", createdAt: twoDaysAgo, completedAt: nil)
-        let mock3 = Item(id: UUID().uuidString, title: "Buy that thing", createdAt: twoDaysAgo, completedAt: yesterday)
-        let mock4 = Item(id: UUID().uuidString, title: "Finish homework", createdAt: yesterday, completedAt: tenMinutesAgo)
-        allItems = [mock1, mock2, mock3, mock4]
-    }
 
 }
